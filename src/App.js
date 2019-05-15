1cf8e9ab3ca74c04accac93d6173a451058fc3b5
asaldivar <ag.saldivar@gmail.com>
2019-05-15 16:24:27 -0700

    set up travis config for staging

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn To Fuck
+          This is STAGING
         </a>
       </header>
     </div>
